a2bc7810c596ffe2d51cca466be8dde2fd408e99
Guilherme Dimas <Guilherme Dimas>
2017-03-18 11:43:11 -0300

    2.3 Desenvolva a classe Animacao

---
+++ js/animacao.js
@@ -0,0 +1,48 @@
+function Animacao(context){
+    this.context = context;
+    this.sprites = [];
+    this.ligado = false;
+}
+
+Animacao.prototype = {
+    novoSprite: function(sprite) {
+        this.sprites.push(sprite);
+    },
+    
+    ligar: function() {
+        this.ligado = true;
+        this.proximoFrame();
+    },
+    
+    desligar: function() {
+        this.ligado = false;
+    },
+    
+    proximoFrame: function() {
+        // Verifica se pode continuar
+        if (!this.ligado) return;
+        
+        // Limpa a tela a cada ciclo
+        this.limparTela();
+        
+        // Atualizamos os estados os sprites
+        for (var i in this.sprites)
+            this.sprites[i].atualizar();
+        
+        // Desenhamos os sprites
+        for (var i in thiss.sprites)
+            this.sprites[i].desenhar();
+        
+        // Chamamos o proximo ciclo
+        var animacao = this;
+        requestAnimationFrame(function() {
+           animacao.proximoFrame(); 
+        });
+    },
+    
+    limparTela: function() {
+        var ctx = this.context;
+        // Limpa o canvas do contexto
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    }
+}
